4f7cb50164b51e4cd788d9326cc0f1c27dd23438
jamaliumair <jamaliumair957@gmail.com>
2024-06-28 03:18:07 +0500

    kfc site committed

---
+++ login/app.js
@@ -0,0 +1,8 @@
+
+function removeBd(el) {
+    el.style.border = "none";
+    // console.log(el);
+}
+
+let insertBd = (inputEl) => {inputEl.style.borderBottom = "1px solid white";}
+let redBd = (inputEl) => {inputEl.style.borderBottom = "1px solid #EA002A";}
